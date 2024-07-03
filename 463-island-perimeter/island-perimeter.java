24785388f328213ff3eeded2eaece44fa94de789
Manojkumar2110 <167198247+Manojkumar2110@users.noreply.github.com>
2024-07-03 11:24:18 +0530

    Time: 4 ms (99.62%) | Memory: 45.4 MB (71.44%) - LeetSync

--- 463-island-perimeter/island-perimeter.java
+++ 463-island-perimeter/island-perimeter.java
@@ -4,13 +4,10 @@ class Solution {
      for(int i=0;i<grid.length;i++){
         for(int j=0;j<grid[0].length;j++){
             if(grid[i][j]==1){
-            peri+=4;
-            if(i<grid.length-1 && grid[i+1][j]==1){
-                peri-=2;
-            }
-            if(j<grid[0].length-1&&grid[i][j+1]==1){
-                peri-=2;
-            }
+                if(i==0||grid[i-1][j]==0) peri++;
+                if(i==grid.length-1||grid[i+1][j]==0) peri++;
+                if(j==0||grid[i][j-1]==0) peri++;
+                if(j==grid[0].length-1||grid[i][j+1]==0) peri++;
             }
         }
      }   
