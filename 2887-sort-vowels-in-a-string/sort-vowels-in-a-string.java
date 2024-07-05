b31e1e05f88b598ed87cb24789de28be896fbe5a
MANOJKUMAR S <167198247+Manojkumar2110@users.noreply.github.com>
2024-07-05 15:19:33 +0530

    Time: 71 ms (29.26%) | Memory: 46.4 MB (58.53%) - LeetSync

--- 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.java
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.java
@@ -1,23 +1,24 @@
 class Solution {
     public String sortVowels(String s) {
-        int n=s.length();
-        char[] arr=new char[n];
-        int k=0;
-        for(int i=0;i<n;i++){
-            if(s.charAt(i)=='A'||s.charAt(i)=='E'||s.charAt(i)=='I'||s.charAt(i)=='O'||s.charAt(i)=='U'||s.charAt(i)=='a'||s.charAt(i)=='e'||s.charAt(i)=='i'||s.charAt(i)=='o'||s.charAt(i)=='u'){
-                arr[k++]=s.charAt(i);
+        char[] vowels={'A','E','I','O','U','a','e','i','o','u'};
+        StringBuilder sc=new StringBuilder(s);
+        ArrayList<Character>ar=new ArrayList<>();
+        for(char c:vowels){
+            ar.add(c);
+        }
+        ArrayList<Character>ar1=new ArrayList<>();
+        for(char c:s.toCharArray()){
+            if(ar.contains(c)){
+            ar1.add(c);
             }
         }
-        char[] vowels = Arrays.copyOf(arr, k);
-        Arrays.sort(vowels);
-        StringBuilder sb=new StringBuilder(s);
-        k=0;
-        for(int i=0;i<n;i++){
-            if(s.charAt(i)=='A'||s.charAt(i)=='E'||s.charAt(i)=='I'||s.charAt(i)=='O'||s.charAt(i)=='U'||s.charAt(i)=='a'||s.charAt(i)=='e'||s.charAt(i)=='i'||s.charAt(i)=='o'||s.charAt(i)=='u'){
-                sb.setCharAt(i, vowels[k++]);
+        Collections.sort(ar1);
+        int index=0;
+        for(int i=0;i<sc.length();i++){
+            if(ar.contains(sc.charAt(i))){
+                sc.setCharAt(i,ar1.get(index++));
+            }
         }
-        }
-        return sb.toString();
-
+        return sc.toString();
     }
 }
