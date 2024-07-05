e459d8f6c23d67fe1658f5cf4cb0ac2a63dd2c6b
MANOJKUMAR S <167198247+Manojkumar2110@users.noreply.github.com>
2024-07-05 11:03:37 +0530

    Time: 9 ms (63.75%) | Memory: 60.8 MB (24.24%) - LeetSync

--- 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.java
+++ 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.java
@@ -1,21 +1,11 @@
 class Solution {
     public int minCost(String colors, int[] neededTime) {
         int res=0;
-        int j=0;
        for(int i=1;i<colors.length();i++){
-            if(colors.charAt(i)==colors.charAt(j)){
-                if(neededTime[i]>neededTime[j]){
-                    res+=neededTime[j];
-                    j=i;
-                }
-                else{
-                    res+=neededTime[i]; 
-                    
-                }
-            }
-            else{
-                j=i;
-            }
+            if(colors.charAt(i)==colors.charAt(i-1)){
+                    res+=Math.min(neededTime[i],neededTime[i-1]);
+                    neededTime[i]=Math.max(neededTime[i],neededTime[i-1]);
+            }    
        }
        return res;
     }
