64da8ac56deac9d432f7f2d24fc39e8e132a5eed
MANOJKUMAR S <167198247+Manojkumar2110@users.noreply.github.com>
2024-07-09 22:52:05 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (92.74%) - LeetSync

--- 27-remove-element/remove-element.java
+++ 27-remove-element/remove-element.java
@@ -3,14 +3,12 @@ class Solution {
         int count=0;
         for(int i=0;i<nums.length;i++){
             if(nums[i]==val){
-                
                 continue;
             }
             else{
                 nums[count]=nums[i];
                 count++;
             }
-
         }
     return count;
     }
