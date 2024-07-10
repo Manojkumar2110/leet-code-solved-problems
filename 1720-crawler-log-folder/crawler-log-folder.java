34471bfdb1b71e37ceb3fbd0ce367dcced3e47b5
MANOJKUMAR S <167198247+Manojkumar2110@users.noreply.github.com>
2024-07-10 14:43:38 +0530

    Time: 1 ms (99.42%) | Memory: 42.6 MB (5.90%) - LeetSync

---
+++ 1720-crawler-log-folder/crawler-log-folder.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minOperations(String[] logs) {
+     int count=0;
+     for(String s:logs){
+        if(s.charAt(0)=='.'&&s.charAt(1)=='/'){
+           continue;
+        }
+        else if(s.charAt(0)==s.charAt(1)&&s.charAt(0)=='.'){
+            if(count!=0){
+                count--;
+            }
+        }
+         else{
+               count++; 
+            }
+        }
+        return count; 
+    }
+}
