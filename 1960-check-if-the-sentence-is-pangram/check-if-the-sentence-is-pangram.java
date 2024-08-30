465707aea3fee42309e771285c5d025812faba7a
MANOJKUMAR S <167198247+Manojkumar2110@users.noreply.github.com>
2024-08-30 09:34:33 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (98.64%) - LeetSync

--- 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
@@ -1,18 +1,13 @@
 class Solution {
     public boolean checkIfPangram(String sentence) {
-       int arr[]=new int[26];
-       for(int i=0;i<sentence.length();i++){
-           arr[sentence.charAt(i)-'a']++; 
-       } 
-       int count=0;
-       for(int j:arr){
-        if(j>0){
-            count++;
+        if(sentence.length()<26){
+            return false;
         }
-       }
-       if(count==26){
+        for(char i='a';i<='z';i++){
+            if(sentence.indexOf(i)==-1){
+                return false;
+            }
+        }
         return true;
-       }
-       return false;
     }
 }
