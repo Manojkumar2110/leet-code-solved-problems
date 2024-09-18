0ffda63b5aae7044569340782cc120bdf45fa18b
MANOJKUMAR S <167198247+Manojkumar2110@users.noreply.github.com>
2024-09-18 08:11:42 +0530

    Time: 5 ms (35.56%) | Memory: 42.6 MB (7.25%) - LeetSync

---
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -0,0 +1,23 @@
+class Solution {
+    static final String[] keypad={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    public List<String> letterCombinations(String digits) {
+        if(digits.isEmpty()){
+            return new ArrayList<>();
+        }
+        return ped("",digits);
+    }
+    static ArrayList<String> ped(String p,String up){
+        if(up.isEmpty()){
+            ArrayList<String>list=new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        int dig=up.charAt(0)-48;
+        ArrayList<String>list=new ArrayList<>();
+        String letters=keypad[dig];
+        for(char ch:letters.toCharArray()){
+            list.addAll(ped(p+ch,up.substring(1)));
+        }
+        return list;
+    }
+}
