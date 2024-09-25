bf423bc42ac766dd1649859551d38ca2fc461e8b
MANOJKUMAR S <167198247+Manojkumar2110@users.noreply.github.com>
2024-09-25 10:01:34 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (16.51%) - LeetSync

--- 1560-number-of-students-doing-homework-at-a-given-time/number-of-students-doing-homework-at-a-given-time.java
+++ 1560-number-of-students-doing-homework-at-a-given-time/number-of-students-doing-homework-at-a-given-time.java
@@ -1,8 +1,8 @@
 class Solution {
     public int busyStudent(int[] startTime, int[] endTime, int queryTime) {
-        if(startTime.length==1&&endTime.length==1&&startTime[0]==queryTime&&endTime[0]==queryTime){
+       /* if(startTime.length==1&&endTime.length==1&&startTime[0]==queryTime&&endTime[0]==queryTime){
             return 1;
-        }
+        }*/
         int count=0;
         for(int i=0;i<startTime.length;i++){
             if(startTime[i]<=queryTime&&queryTime<=endTime[i]){
