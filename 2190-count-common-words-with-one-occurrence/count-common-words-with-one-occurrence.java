1fc6011e68634c24c314918bbc5509f7f6f6a661
MANOJKUMAR S <167198247+Manojkumar2110@users.noreply.github.com>
2024-09-30 15:31:19 +0530

    Time: 7 ms (52.86%) | Memory: 44 MB (76.65%) - LeetSync

--- 2190-count-common-words-with-one-occurrence/count-common-words-with-one-occurrence.java
+++ 2190-count-common-words-with-one-occurrence/count-common-words-with-one-occurrence.java
@@ -1,33 +1,27 @@
+
 class Solution {
     public int countWords(String[] words1, String[] words2) {
-       HashMap<String,Integer>hm=new HashMap<>();
-       for(int i=0;i<words1.length;i++){
-            if(!hm.containsKey(words1[i])){
-                hm.put(words1[i],1);
-            }
-            else{
-                hm.put(words1[i],hm.get(words1[i])+1);
-            }
-       } 
-       HashMap<String,Integer>hm1=new HashMap<>();
-       for(int i=0;i<words2.length;i++){
-            if(!hm1.containsKey(words2[i])){
-                hm1.put(words2[i],1);
-            }
-            else{
-                hm1.put(words2[i],hm1.get(words2[i])-1);
-            }
-       }
-       int count=0;
-       for(int i=0;i<words1.length;i++){
-        for(int j=0;j<words2.length;j++){
-            if(words1[i].equals(words2[j])){
-                if(hm.get(words1[i])==1&&hm1.get(words2[j])==1){
-                    count++;
-                }
+        HashMap<String, Integer> countMap1 = new HashMap<>();
+        HashMap<String, Integer> countMap2 = new HashMap<>();
+
+        // Count occurrences in words1
+        for (String word : words1) {
+            countMap1.put(word, countMap1.getOrDefault(word, 0) + 1);
+        }
+
+        // Count occurrences in words2
+        for (String word : words2) {
+            countMap2.put(word, countMap2.getOrDefault(word, 0) + 1);
+        }
+
+        // Count words that appear exactly once in both arrays
+        int count = 0;
+        for (String word : countMap1.keySet()) {
+            if (countMap1.get(word) == 1 && countMap2.getOrDefault(word, 0) == 1) {
+                count++;
             }
         }
-       }
-       return count;
+
+        return count;
     }
 }
