f05949001d7cea3411e386d44943b7c1f4b11b50
MANOJKUMAR S <167198247+Manojkumar2110@users.noreply.github.com>
2024-10-29 12:05:49 +0530

    Time: 3 ms (73.58%) | Memory: 41.1 MB (67.05%) - LeetSync

--- 2824-check-if-the-number-is-fascinating/check-if-the-number-is-fascinating.java
+++ 2824-check-if-the-number-is-fascinating/check-if-the-number-is-fascinating.java
@@ -1,18 +1,32 @@
 class Solution {
     public boolean isFascinating(int n) {
-       String concat=""+n+(n*2)+(n*3);
-       int[] count=new int[10];
-       for(char c:concat.toCharArray()){
-            count[c-'0']++;
-       } 
-       if(count[0]>0||concat.length()!=9){
-        return false;
-       }
-       for(int i=1;i<9;i++){
-        if(count[i]!=1){
+       ArrayList<Integer>al=new ArrayList<>();
+        int a=n*2;
+        int b=n*3;
+        while(n>0){
+            int res=n%10;
+            al.add(res);
+            n/=10;
+        }
+        while(a>0){
+            int res=a%10;
+            al.add(res);
+            a/=10;
+        }
+        while(b>0){
+            int res=b%10;
+            al.add(res);
+            b/=10;
+        }
+        Collections.sort(al);
+        if(al.size()>9){
             return false;
         }
-       }
-       return true;
+        for(int i=1;i<=9;i++){
+            if(!al.contains(i)){
+                return false;
+            }
+        }
+        return true;
     }
 }
