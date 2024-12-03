15330918861dce58ade244d5685d878ac2924f19
MANOJKUMAR S <167198247+Manojkumar2110@users.noreply.github.com>
2024-12-03 13:09:36 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (41.90%) - LeetSync

--- 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.java
+++ 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.java
@@ -4,10 +4,9 @@ class Solution {
         int k=-1;
         for(int i=0;i<arr.length;i++){
             if(arr[i].startsWith(searchWord)){
-                k=i+1;
-                break;
+               return i+1;
             }
         }
-        return k;
+        return -1;
     }
 }
