59c9b7a36e49029ce9427c659571edbd2323bc92
MANOJKUMAR S <167198247+Manojkumar2110@users.noreply.github.com>
2025-02-18 14:05:02 +0530

    Time: 1 ms (75.25%) | Memory: 41.7 MB (62.62%) - LeetSync

--- 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.java
+++ 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.java
@@ -1,33 +1,15 @@
 class Solution {
     public String smallestNumber(String pattern) {
-        int n=pattern.length()+1;
-        int arr[]=new int[n];
-        for(int i=0;i<n;i++){
-            arr[i]=i+1;
-        }
+        Stack<Integer>st=new Stack<>();
         StringBuilder sb=new StringBuilder();
-        for(int i=0;i<pattern.length();i++){
-            int temp=i;
-            while(temp<pattern.length()&&pattern.charAt(temp)=='D'){
-                temp++;
+        for(int i=0;i<=pattern.length();i++){
+            st.push(i+1);
+            if(i==pattern.length()||pattern.charAt(i)=='I'){
+                while(!st.isEmpty()){
+                    sb.append(st.pop());
+                }
             }
-            swap(arr,i,temp);
-            if(temp!=i){
-                i=temp-1;
-            }
-        }
-        for(int a:arr){
-            sb.append(a);
         }
         return sb.toString();
-    }
-    private void swap(int arr[],int st,int end){
-        while(st<end){
-            int temp=arr[st];
-            arr[st]=arr[end];
-            arr[end]=temp;
-            st++;
-            end--;
-        }
     }
 }
