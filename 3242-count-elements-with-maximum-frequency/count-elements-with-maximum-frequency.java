175c4c0a30130a0e3a9597bc93ef5ce2334999c5
MANOJKUMAR S <167198247+Manojkumar2110@users.noreply.github.com>
2025-09-22 19:05:52 +0530

    Time: 2 ms (75.64%) | Memory: 42.5 MB (32.20%) - LeetSync

--- 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.java
+++ 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.java
@@ -1,27 +1,27 @@
 class Solution {
-    public int maxFrequencyElements(int[] nums) {
-        HashMap<Integer,Integer>hm=new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            if(!hm.containsKey(nums[i])){
-                hm.put(nums[i],1);
-            }else{
-                hm.put(nums[i],hm.get(nums[i])+1);
-            }
-        }
-        int count=0;
-        int max=0;
-        for(int value : hm.values()){
-             max = Math.max(max,value);
-         }
-        for(int k:hm.values()){
-            if(k>=max && k>1){
-                count+=k;
-            }
-        }
-        if(count>1){
-            return count;
-        }else{
-            return nums.length;
-        }
-    }
-}
+        public int maxFrequencyElements(int[] nums) {
+                HashMap<Integer,Integer>hm=new HashMap<>();
+                        for(int i=0;i<nums.length;i++){
+                                    if(!hm.containsKey(nums[i])){
+                                                    hm.put(nums[i],1);
+                                                                }else{
+                                                                                hm.put(nums[i],hm.get(nums[i])+1);
+                                                                                            }
+                                                                                                    }
+                                                                                                            int count=0;
+                                                                                                                    int max=0;
+                                                                                                                            for(int value : hm.values()){
+                                                                                                                                         max = Math.max(max,value);
+                                                                                                                                                  }
+                                                                                                                                                          for(int k:hm.values()){
+                                                                                                                                                                      if(k>=max && k>1){
+                                                                                                                                                                                      count+=k;
+                                                                                                                                                                                                  }
+                                                                                                                                                                                                          }
+                                                                                                                                                                                                                  if(count>1){
+                                                                                                                                                                                                                              return count;
+                                                                                                                                                                                                                                      }else{
+                                                                                                                                                                                                                                                  return nums.length;
+                                                                                                                                                                                                                                                          }
+                                                                                                                                                                                                                                                              }
+                                                                                                                                                                                                                                                              }
